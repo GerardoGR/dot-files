b2c25cb5217153e46a1e40c41fac4b2b6e836fd2
Gerardo Gomez <gerardo.gomez@tutanota.com>
2022-05-15 11:39:43 +0200

    docs: document install script

--- local-bin/dot-files-install.bash
+++ local-bin/dot-files-install.bash
@@ -2,24 +2,30 @@
 
 set -e
 
-# Setup dot-files
+# Common bash
 ln -sf $HOME/.dot-files/bashrc $HOME/.bashrc
 ln -sf $HOME/.dot-files/bash_profile $HOME/.bash_profile
 
+# local bin paths
 mkdir -p $HOME/.local
 rm -rf $HOME/.local/dot-files-bin
 ln -sf $HOME/.dot-files/local-bin $HOME/.local/dot-files-bin
 
+# vim
 rm -rf $HOME/.vim
 ln -sf $HOME/.dot-files/vim $HOME/.vim
 
+# tmux
 mkdir -p $HOME/.tmux/
 rm -rf $HOME/.tmux/plugins
 ln -sf $HOME/.dot-files/tmux/plugins $HOME/.tmux/plugins
 ln -sf $HOME/.dot-files/tmux/tmux.conf $HOME/.tmux.conf
 
+# asdf
 rm -rf $HOME/.asdf
 ln -sf $HOME/.dot-files/asdf $HOME/.asdf
 ln -sf $HOME/.dot-files/asdf-tool-versions $HOME/.tool-versions
+
+# asdf - install development environment
 source $HOME/.bashrc
 asdf install
