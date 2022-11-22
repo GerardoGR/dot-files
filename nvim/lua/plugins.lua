b3b5a86a22237ac6d59b7e0d3e1c0d15f9e09912
Gerardo Gomez <gerardo.gomez@tutanota.com>
2022-11-22 21:45:37 +0100

    feat: bootstrap packer.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,6 +5,20 @@ vim.cmd([[
   augroup end
 ]])
 
+-- Taken from: https://github.com/wbthomason/packer.nvim#bootstrapping
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- Packer can manage itself
  
@@ -51,4 +65,8 @@ return require('packer').startup(function(use)
       end,
       requires = { "nvim-lua/plenary.nvim" },
   })
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
